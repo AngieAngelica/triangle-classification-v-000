4e2eac029bedfe2ef2b9dcfd2cccc65ff4135d83
Angie Kassab <anasab12@gmail.com>
2018-08-31 15:46:41 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,7 @@ class Triangle
     triangle2 = @side1 + @side3 - @side2
     triangle3 = @side1 + @side2 - @side3
 
-    if diff1 <= 0 || diff2 <= 0 || diff3 <= 0 || @side1 <= 0 || @side2 <= 0 || @side3 <= 0
+    if triangle1 <= 0 || trangle2 <= 0 || triangle3 <= 0 || @side1 <= 0 || @side2 <= 0 || @side3 <= 0
       raise TriangleError, "This is not a valid triangle!"
     elsif @side1 == @side2 && @side1 == @side3
       :equilateral
